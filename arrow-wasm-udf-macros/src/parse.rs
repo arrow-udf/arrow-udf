11abae9ff1360d60d9cb09d1da16a15890e39a8b
Runji Wang <wangrunji0408@163.com>
2023-12-11 21:33:09 +0800

    copy `risingwave_expr_macros`
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

---
+++ arrow-wasm-udf-macros/src/parse.rs
@@ -0,0 +1,326 @@
+// Copyright 2023 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Parse the tokens of the macro.
+
+use quote::ToTokens;
+use syn::parse::{Parse, ParseStream};
+use syn::spanned::Spanned;
+use syn::{LitStr, Token};
+
+use super::*;
+
+impl Parse for FunctionAttr {
+    /// Parse the attribute of the function macro.
+    fn parse(input: ParseStream<'_>) -> Result<Self> {
+        let mut parsed = Self::default();
+
+        let sig = input.parse::<LitStr>()?;
+        let sig_str = sig.value();
+        let (name_args, ret) = match sig_str.split_once("->") {
+            Some((name_args, ret)) => (name_args, ret),
+            None => (sig_str.as_str(), "void"),
+        };
+        let (name, args) = name_args
+            .split_once('(')
+            .ok_or_else(|| Error::new_spanned(&sig, "expected '('"))?;
+        let args = args.trim_start().trim_end_matches([')', ' ']);
+        let (is_table_function, ret) = match ret.trim_start().strip_prefix("setof") {
+            Some(s) => (true, s),
+            None => (false, ret),
+        };
+        parsed.name = name.trim().to_string();
+        parsed.args = if args.is_empty() {
+            vec![]
+        } else {
+            args.split(',').map(|s| s.trim().to_string()).collect()
+        };
+        parsed.ret = ret.trim().to_string();
+        parsed.is_table_function = is_table_function;
+
+        if input.parse::<Token![,]>().is_err() {
+            return Ok(parsed);
+        }
+
+        let metas = input.parse_terminated(syn::Meta::parse, Token![,])?;
+        for meta in metas {
+            let get_value = || {
+                let kv = meta.require_name_value()?;
+                let syn::Expr::Lit(lit) = &kv.value else {
+                    return Err(Error::new(kv.value.span(), "expected literal"));
+                };
+                let syn::Lit::Str(lit) = &lit.lit else {
+                    return Err(Error::new(kv.value.span(), "expected string literal"));
+                };
+                Ok(lit.value())
+            };
+            if meta.path().is_ident("batch_fn") {
+                parsed.batch_fn = Some(get_value()?);
+            } else if meta.path().is_ident("state") {
+                parsed.state = Some(get_value()?);
+            } else if meta.path().is_ident("init_state") {
+                parsed.init_state = Some(get_value()?);
+            } else if meta.path().is_ident("prebuild") {
+                parsed.prebuild = Some(get_value()?);
+            } else if meta.path().is_ident("type_infer") {
+                parsed.type_infer = Some(get_value()?);
+            } else if meta.path().is_ident("generic") {
+                parsed.generic = Some(get_value()?);
+            } else if meta.path().is_ident("volatile") {
+                parsed.volatile = true;
+            } else if meta.path().is_ident("deprecated") || meta.path().is_ident("internal") {
+                parsed.deprecated = true;
+            } else if meta.path().is_ident("rewritten") {
+                parsed.rewritten = true;
+            } else if meta.path().is_ident("append_only") {
+                parsed.append_only = true;
+            } else {
+                return Err(Error::new(
+                    meta.span(),
+                    format!("invalid property: {:?}", meta.path()),
+                ));
+            }
+        }
+        Ok(parsed)
+    }
+}
+
+impl Parse for UserFunctionAttr {
+    fn parse(input: ParseStream<'_>) -> Result<Self> {
+        let itemfn: syn::ItemFn = input.parse()?;
+        Ok(UserFunctionAttr::from(&itemfn.sig))
+    }
+}
+
+impl From<&syn::Signature> for UserFunctionAttr {
+    fn from(sig: &syn::Signature) -> Self {
+        let (return_type_kind, iterator_item_kind, core_return_type) = match &sig.output {
+            syn::ReturnType::Default => (ReturnTypeKind::T, None, "()".into()),
+            syn::ReturnType::Type(_, ty) => {
+                let (kind, inner) = check_type(ty);
+                match strip_iterator(inner) {
+                    Some(ty) => {
+                        let (inner_kind, inner) = check_type(ty);
+                        (kind, Some(inner_kind), inner.to_token_stream().to_string())
+                    }
+                    None => (kind, None, inner.to_token_stream().to_string()),
+                }
+            }
+        };
+        UserFunctionAttr {
+            name: sig.ident.to_string(),
+            async_: sig.asyncness.is_some(),
+            write: sig.inputs.iter().any(arg_is_write),
+            context: sig.inputs.iter().any(arg_is_context),
+            retract: last_arg_is_retract(sig),
+            arg_option: args_contain_option(sig),
+            first_mut_ref_arg: first_mut_ref_arg(sig),
+            return_type_kind,
+            iterator_item_kind,
+            core_return_type,
+            generic: sig.generics.params.len(),
+            return_type_span: sig.output.span(),
+        }
+    }
+}
+
+impl Parse for AggregateImpl {
+    fn parse(input: ParseStream<'_>) -> Result<Self> {
+        let itemimpl: syn::ItemImpl = input.parse()?;
+        let parse_function = |name: &str| {
+            itemimpl.items.iter().find_map(|item| match item {
+                syn::ImplItem::Fn(syn::ImplItemFn { sig, .. }) if sig.ident == name => {
+                    Some(UserFunctionAttr::from(sig))
+                }
+                _ => None,
+            })
+        };
+        let self_path = itemimpl.self_ty.to_token_stream().to_string();
+        let struct_name = match self_path.split_once('<') {
+            Some((path, _)) => path.trim().into(), // remove generic parameters
+            None => self_path,
+        };
+        Ok(AggregateImpl {
+            struct_name,
+            accumulate: parse_function("accumulate").expect("expect accumulate function"),
+            retract: parse_function("retract"),
+            merge: parse_function("merge"),
+            finalize: parse_function("finalize"),
+            create_state: parse_function("create_state"),
+            encode_state: parse_function("encode_state"),
+            decode_state: parse_function("decode_state"),
+        })
+    }
+}
+
+impl Parse for AggregateFnOrImpl {
+    fn parse(input: ParseStream<'_>) -> Result<Self> {
+        // consume attributes
+        let _ = input.call(syn::Attribute::parse_outer)?;
+        if input.peek(Token![impl]) {
+            Ok(AggregateFnOrImpl::Impl(input.parse()?))
+        } else {
+            Ok(AggregateFnOrImpl::Fn(input.parse()?))
+        }
+    }
+}
+
+/// Check if the argument is `&mut impl Write`.
+fn arg_is_write(arg: &syn::FnArg) -> bool {
+    let syn::FnArg::Typed(arg) = arg else {
+        return false;
+    };
+    let syn::Type::Reference(syn::TypeReference { elem, .. }) = arg.ty.as_ref() else {
+        return false;
+    };
+    let syn::Type::ImplTrait(syn::TypeImplTrait { bounds, .. }) = elem.as_ref() else {
+        return false;
+    };
+    let Some(syn::TypeParamBound::Trait(syn::TraitBound { path, .. })) = bounds.first() else {
+        return false;
+    };
+    let Some(seg) = path.segments.last() else {
+        return false;
+    };
+    seg.ident == "Write"
+}
+
+/// Check if the argument is `&Context`.
+fn arg_is_context(arg: &syn::FnArg) -> bool {
+    let syn::FnArg::Typed(arg) = arg else {
+        return false;
+    };
+    let syn::Type::Reference(syn::TypeReference { elem, .. }) = arg.ty.as_ref() else {
+        return false;
+    };
+    let syn::Type::Path(path) = elem.as_ref() else {
+        return false;
+    };
+    let Some(seg) = path.path.segments.last() else {
+        return false;
+    };
+    seg.ident == "Context"
+}
+
+/// Check if the last argument is `retract: bool`.
+fn last_arg_is_retract(sig: &syn::Signature) -> bool {
+    let Some(syn::FnArg::Typed(arg)) = sig.inputs.last() else {
+        return false;
+    };
+    let syn::Pat::Ident(pat) = &*arg.pat else {
+        return false;
+    };
+    pat.ident.to_string().contains("retract")
+}
+
+/// Check if any argument is `Option`.
+fn args_contain_option(sig: &syn::Signature) -> bool {
+    for arg in &sig.inputs {
+        let syn::FnArg::Typed(arg) = arg else {
+            continue;
+        };
+        let syn::Type::Path(path) = arg.ty.as_ref() else {
+            continue;
+        };
+        let Some(seg) = path.path.segments.last() else {
+            continue;
+        };
+        if seg.ident == "Option" {
+            return true;
+        }
+    }
+    false
+}
+
+/// Returns `T` if the first argument (except `self`) is `&mut T`.
+fn first_mut_ref_arg(sig: &syn::Signature) -> Option<String> {
+    let arg = match sig.inputs.first()? {
+        syn::FnArg::Typed(arg) => arg,
+        syn::FnArg::Receiver(_) => match sig.inputs.iter().nth(1)? {
+            syn::FnArg::Typed(arg) => arg,
+            _ => return None,
+        },
+    };
+    let syn::Type::Reference(syn::TypeReference {
+        elem,
+        mutability: Some(_),
+        ..
+    }) = arg.ty.as_ref()
+    else {
+        return None;
+    };
+    Some(elem.to_token_stream().to_string())
+}
+
+/// Check the return type.
+fn check_type(ty: &syn::Type) -> (ReturnTypeKind, &syn::Type) {
+    if let Some(inner) = strip_outer_type(ty, "Result") {
+        if let Some(inner) = strip_outer_type(inner, "Option") {
+            (ReturnTypeKind::ResultOption, inner)
+        } else {
+            (ReturnTypeKind::Result, inner)
+        }
+    } else if let Some(inner) = strip_outer_type(ty, "Option") {
+        (ReturnTypeKind::Option, inner)
+    } else if let Some(inner) = strip_outer_type(ty, "DatumRef") {
+        (ReturnTypeKind::Option, inner)
+    } else {
+        (ReturnTypeKind::T, ty)
+    }
+}
+
+/// Check if the type is `type_<T>` and return `T`.
+fn strip_outer_type<'a>(ty: &'a syn::Type, type_: &str) -> Option<&'a syn::Type> {
+    let syn::Type::Path(path) = ty else {
+        return None;
+    };
+    let Some(seg) = path.path.segments.last() else {
+        return None;
+    };
+    if seg.ident != type_ {
+        return None;
+    }
+    let syn::PathArguments::AngleBracketed(args) = &seg.arguments else {
+        return None;
+    };
+    let Some(syn::GenericArgument::Type(ty)) = args.args.first() else {
+        return None;
+    };
+    Some(ty)
+}
+
+/// Check if the type is `impl Iterator<Item = T>` and return `T`.
+fn strip_iterator(ty: &syn::Type) -> Option<&syn::Type> {
+    let syn::Type::ImplTrait(impl_trait) = ty else {
+        return None;
+    };
+    let syn::TypeParamBound::Trait(trait_bound) = impl_trait.bounds.first()? else {
+        return None;
+    };
+    let segment = trait_bound.path.segments.last().unwrap();
+    if segment.ident != "Iterator" {
+        return None;
+    }
+    let syn::PathArguments::AngleBracketed(angle_bracketed) = &segment.arguments else {
+        return None;
+    };
+    for arg in &angle_bracketed.args {
+        if let syn::GenericArgument::AssocType(b) = arg
+            && b.ident == "Item"
+        {
+            return Some(&b.ty);
+        }
+    }
+    None
+}
---
+++ arrow-wasm-udf-macros/src/types.rs
@@ -0,0 +1,149 @@
+// Copyright 2023 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! This module provides utility functions for SQL data type conversion and manipulation.
+
+//  name        data type   array type          owned type      ref type            primitive
+const TYPE_MATRIX: &str = "
+    boolean     Boolean     BoolArray           bool            bool                _
+    int2        Int16       I16Array            i16             i16                 y
+    int4        Int32       I32Array            i32             i32                 y
+    int8        Int64       I64Array            i64             i64                 y
+    int256      Int256      Int256Array         Int256          Int256Ref<'_>       _
+    float4      Float32     F32Array            F32             F32                 y
+    float8      Float64     F64Array            F64             F64                 y
+    decimal     Decimal     DecimalArray        Decimal         Decimal             y
+    serial      Serial      SerialArray         Serial          Serial              y
+    date        Date        DateArray           Date            Date                y
+    time        Time        TimeArray           Time            Time                y
+    timestamp   Timestamp   TimestampArray      Timestamp       Timestamp           y
+    timestamptz Timestamptz TimestamptzArray    Timestamptz     Timestamptz         y
+    interval    Interval    IntervalArray       Interval        Interval            y
+    varchar     Varchar     Utf8Array           Box<str>        &str                _
+    bytea       Bytea       BytesArray          Box<[u8]>       &[u8]               _
+    jsonb       Jsonb       JsonbArray          JsonbVal        JsonbRef<'_>        _
+    anyarray    List        ListArray           ListValue       ListRef<'_>         _
+    struct      Struct      StructArray         StructValue     StructRef<'_>       _
+    any         ???         ArrayImpl           ScalarImpl      ScalarRefImpl<'_>   _
+";
+
+/// Maps a data type to its corresponding data type name.
+pub fn data_type(ty: &str) -> &str {
+    lookup_matrix(ty, 1)
+}
+
+/// Maps a data type to its corresponding array type name.
+pub fn array_type(ty: &str) -> &str {
+    lookup_matrix(ty, 2)
+}
+
+/// Maps a data type to its corresponding `Scalar` type name.
+pub fn owned_type(ty: &str) -> &str {
+    lookup_matrix(ty, 3)
+}
+
+/// Maps a data type to its corresponding `ScalarRef` type name.
+pub fn ref_type(ty: &str) -> &str {
+    lookup_matrix(ty, 4)
+}
+
+/// Checks if a data type is primitive.
+pub fn is_primitive(ty: &str) -> bool {
+    lookup_matrix(ty, 5) == "y"
+}
+
+fn lookup_matrix(mut ty: &str, idx: usize) -> &str {
+    if ty.ends_with("[]") {
+        ty = "anyarray";
+    } else if ty.starts_with("struct") {
+        ty = "struct";
+    } else if ty == "void" {
+        // XXX: we don't support void type yet.
+        //      replace it with int for now.
+        ty = "int4";
+    }
+    let s = TYPE_MATRIX.trim().lines().find_map(|line| {
+        let mut parts = line.split_whitespace();
+        if parts.next() == Some(ty) {
+            Some(parts.nth(idx - 1).unwrap())
+        } else {
+            None
+        }
+    });
+    s.unwrap_or_else(|| panic!("unknown type: {}", ty))
+}
+
+/// Expands a type wildcard string into a list of concrete types.
+pub fn expand_type_wildcard(ty: &str) -> Vec<&str> {
+    match ty {
+        "*" => TYPE_MATRIX
+            .trim()
+            .lines()
+            .map(|l| l.split_whitespace().next().unwrap())
+            .filter(|l| *l != "any")
+            .collect(),
+        "*int" => vec!["int2", "int4", "int8"],
+        "*float" => vec!["float4", "float8"],
+        _ => vec![ty],
+    }
+}
+
+/// Computes the minimal compatible type between a pair of data types.
+///
+/// This function is used to determine the `auto` type.
+pub fn min_compatible_type(types: &[impl AsRef<str>]) -> &str {
+    if types.len() == 1 {
+        return types[0].as_ref();
+    }
+    assert_eq!(types.len(), 2);
+    match (types[0].as_ref(), types[1].as_ref()) {
+        (a, b) if a == b => a,
+
+        ("int2", "int2") => "int2",
+        ("int2", "int4") => "int4",
+        ("int2", "int8") => "int8",
+
+        ("int4", "int2") => "int4",
+        ("int4", "int4") => "int4",
+        ("int4", "int8") => "int8",
+
+        ("int8", "int2") => "int8",
+        ("int8", "int4") => "int8",
+        ("int8", "int8") => "int8",
+
+        ("int2", "int256") => "int256",
+        ("int4", "int256") => "int256",
+        ("int8", "int256") => "int256",
+        ("int256", "int2") => "int256",
+        ("int256", "int4") => "int256",
+        ("int256", "int8") => "int256",
+        ("int256", "float8") => "float8",
+        ("float8", "int256") => "float8",
+
+        ("float4", "float4") => "float4",
+        ("float4", "float8") => "float8",
+
+        ("float8", "float4") => "float8",
+        ("float8", "float8") => "float8",
+
+        ("decimal", "decimal") => "decimal",
+
+        ("date", "timestamp") => "timestamp",
+        ("timestamp", "date") => "timestamp",
+        ("time", "interval") => "interval",
+        ("interval", "time") => "interval",
+
+        (a, b) => panic!("unknown minimal compatible type for {a:?} and {b:?}"),
+    }
+}

8a98be0de6a2b5455e5c2e938e071eda0ca75447
github-action-benchmark <github@users.noreply.github.com>
2023-12-14 04:37:03 +0000

    add Rust Benchmark (cargo) benchmark result for 4b898ddc346a7bdf95024cac07a9f4d95163eae4

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1702528623664,
+  "repoUrl": "https://github.com/risingwavelabs/arrow-udf-wasm",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "wangrunji0408@163.com",
+            "name": "Runji Wang",
+            "username": "wangrunji0408"
+          },
+          "committer": {
+            "email": "wangrunji0408@163.com",
+            "name": "Runji Wang",
+            "username": "wangrunji0408"
+          },
+          "distinct": true,
+          "id": "4b898ddc346a7bdf95024cac07a9f4d95163eae4",
+          "message": "add Github Actions\n\nSigned-off-by: Runji Wang <wangrunji0408@163.com>",
+          "timestamp": "2023-12-13T23:07:47+08:00",
+          "tree_id": "fafa1d4ffc5641d0bade7d433299bc740c2aaa8e",
+          "url": "https://github.com/risingwavelabs/arrow-udf-wasm/commit/4b898ddc346a7bdf95024cac07a9f4d95163eae4"
+        },
+        "date": 1702528623293,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "gcd/native",
+            "value": 3904,
+            "range": "± 7",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}

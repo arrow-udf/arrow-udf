f652386fbadc73b1dec8b7c1dcf76b92d1ce418e
github-action-benchmark <github@users.noreply.github.com>
2023-12-14 15:45:52 +0000

    add Rust Benchmark (cargo) benchmark result for 5482dc630cd5af88578192b5ef4f46cb9e3ef72c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702556403713,
+  "lastUpdate": 1702568752684,
   "repoUrl": "https://github.com/risingwavelabs/arrow-udf-wasm",
   "entries": {
     "Rust Benchmark": [
@@ -89,6 +89,36 @@ window.BENCHMARK_DATA = {
             "name": "gcd/native",
             "value": 3903,
             "range": "± 4",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "wangrunji0408@163.com",
+            "name": "Runji Wang",
+            "username": "wangrunji0408"
+          },
+          "committer": {
+            "email": "wangrunji0408@163.com",
+            "name": "Runji Wang",
+            "username": "wangrunji0408"
+          },
+          "distinct": true,
+          "id": "5482dc630cd5af88578192b5ef4f46cb9e3ef72c",
+          "message": "support returning nested struct\n\nSigned-off-by: Runji Wang <wangrunji0408@163.com>",
+          "timestamp": "2023-12-14T23:40:23+08:00",
+          "tree_id": "58b967b5abb8612ab4ec3d70e83753fc29f7e127",
+          "url": "https://github.com/risingwavelabs/arrow-udf-wasm/commit/5482dc630cd5af88578192b5ef4f46cb9e3ef72c"
+        },
+        "date": 1702568752074,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "gcd/native",
+            "value": 3904,
+            "range": "± 10",
             "unit": "ns/iter"
           }
         ]

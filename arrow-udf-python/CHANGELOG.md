57620b3c7c3c7dfcf16a71dcb2dd4d10444324a6
Runji Wang <wangrunji0408@163.com>
2024-05-23 21:11:57 +0800

    fix building arrow-udf-python on docs.rs (#29)
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- arrow-udf-python/CHANGELOG.md
+++ arrow-udf-python/CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.2.1] - 2024-05-23
+
+### Fixed
+
+- Fix building documentation on docs.rs.
+
 ## [0.2.0] - 2024-05-23
 
 ### Added
--- arrow-udf-python/build.rs
+++ arrow-udf-python/build.rs
@@ -1,6 +1,10 @@
 use pyo3_build_config::PythonVersion;
 
 fn main() {
+    // skip the check on docs.rs. it only has python 3.10.
+    if std::env::var("DOCS_RS").is_ok() {
+        return;
+    }
     let version = pyo3_build_config::get().version;
     let minimum_version = PythonVersion {
         major: 3,

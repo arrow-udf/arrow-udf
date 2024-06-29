efbf9145209dde54f5fb73dc3db63ad79626942d
Chojan Shang <psiace@apache.org>
2024-06-29 12:04:00 +0800

    chore: make clippy happy
    Signed-off-by: Chojan Shang <psiace@apache.org>

--- arrow-udf-wasm/src/lib.rs
+++ arrow-udf-wasm/src/lib.rs
@@ -92,7 +92,7 @@ impl Runtime {
     pub fn with_config(binary: &[u8], config: Config) -> Result<Self> {
         // use a global engine by default
         static ENGINE: once_cell::sync::Lazy<Engine> =
-            once_cell::sync::Lazy::new(|| Engine::default());
+            once_cell::sync::Lazy::new(Engine::default);
         Self::with_config_engine(binary, config, &ENGINE)
     }
 

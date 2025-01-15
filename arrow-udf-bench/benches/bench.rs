68f087c964ca6407fb828ec8b242badf3ba7a63b
baishen <baishen2009@gmail.com>
2025-01-15 22:02:23 +0800

    fix

--- arrow-udf-bench/benches/bench.rs
+++ arrow-udf-bench/benches/bench.rs
@@ -23,7 +23,6 @@ use arrow_udf_python::Runtime as PythonRuntime;
 use arrow_udf_wasm::Runtime as WasmRuntime;
 use criterion::{criterion_group, criterion_main, Criterion};
 
-#[allow(unexpected_cfgs)]
 fn bench_eval_gcd(c: &mut Criterion) {
     #[function("gcd(int, int) -> int")]
     fn gcd(mut a: i32, mut b: i32) -> i32 {
@@ -182,7 +181,6 @@ def range1(n: int):
     });
 }
 
-#[allow(unexpected_cfgs)]
 fn bench_eval_decimal(c: &mut Criterion) {
     #[function("decimal(decimal) -> decimal")]
     fn decimal<T>(a: T) -> T {
--- arrow-udf-example/src/lib.rs
+++ arrow-udf-example/src/lib.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![allow(unexpected_cfgs)]
-
 use arrow_udf::{function, types::*};
 
 #[function("gcd(int, int) -> int")]

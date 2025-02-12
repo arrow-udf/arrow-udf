50dab6ac90c9975a5ac907b162a7406367c603b1
Runji Wang <wangrunji0408@163.com>
2025-02-12 23:54:51 +0800

    writer style
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- arrow-udf-duckdb-example/src/lib.rs
+++ arrow-udf-duckdb-example/src/lib.rs
@@ -2,10 +2,11 @@ use arrow_udf::function;
 use duckdb::{ffi, Connection, Result};
 use duckdb_loadable_macros::duckdb_entrypoint_c_api;
 use std::error::Error;
+use std::fmt::Write;
 
 #[function("hello(varchar) -> varchar", duckdb = "Hello")]
-fn hello(name: &str) -> String {
-    format!("Hello {}!", name)
+fn hello(name: &str, writer: &mut impl Write) {
+    write!(writer, "Hello {}!", name).unwrap();
 }
 
 #[duckdb_entrypoint_c_api(ext_name = "arrow_udf_duckdb_example", min_duckdb_version = "v1.0.0")]

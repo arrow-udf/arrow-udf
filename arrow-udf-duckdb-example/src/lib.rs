8d2903d6d8b35cb11cc7bfae6edd9fcfe896b0b8
Runji Wang <wangrunji0408@163.com>
2025-08-09 15:17:03 +0800

    disable buggy function
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- arrow-udf-duckdb-example/src/lib.rs
+++ arrow-udf-duckdb-example/src/lib.rs
@@ -24,8 +24,9 @@ fn generate_numbers(n: i32) -> impl Iterator<Item = i32> {
 pub unsafe fn extension_entrypoint(con: Connection) -> Result<(), Box<dyn Error>> {
     con.register_scalar_function::<Hello>("hello")
         .expect("Failed to register hello function");
-    con.register_scalar_function::<SplitWords>("split_words")
-        .expect("Failed to register split_words function");
+    // waiting for bug fix: https://github.com/duckdb/duckdb-rs/pull/574
+    // con.register_scalar_function::<SplitWords>("split_words")
+    //     .expect("Failed to register split_words function");
     con.register_table_function::<GenerateNumbers>("generate_numbers")
         .expect("Failed to register generate_numbers table function");
     Ok(())
